93ed31222a6b08ddaa426e153df9b585eec24bce
InvizHer <112791490+InvizHer@users.noreply.github.com>
2023-08-13 17:26:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ class Config:
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
     CAPTION = os.environ.get("CAPTION", "")
     FROM_CHANNEL = os.environ.get("FROM_CHANNEL", "@OkFiles")
-    FILTER_TYPE = os.environ.get("FILTER_TYPE", "video")
+    FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
     OWNER_ID = os.environ.get("OWNER_ID", "2056407064")
     LIMIT = int(os.environ.get("LIMIT", "1000000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
-    SESSION = os.environ.get("SESSION", "BQBCa_j9IuaXuwWqmX6aCxAmLllIdj5Dus00gUDw3ekpZiSOIuSl4xY3mib0gjAIaG-GXI02IsHRJYFnZftfwElFU4R1DaZxIQjkhqvosMIL47ixaKaAtoZcHlmjbtgx8-16XBdwhGc2ECm8jmTdLxi76vj72ijo3bi_QndSdKM3mrZegULpDMVAb8egASKytZmH0CgQrpg3f3LllRquIG3vQ-Lxh51O4hIXut9X3f3a_aycHgGus7UEIadYVneoKV8DiDy51cI_Sv9FNSOsAfYe10YNbdIMNh1RyBoOgCjT6aQaAFvTMzjCbsSNeE1DcsdXgy2G5hfM6WWpayrlWXlmAAAAAWFUxV8A")
+    SESSION = os.environ.get("SESSION", "BQE45_EAMODUWefKh2pAjdotR6sSHf-lsJs2zqQmQZh-ALg1QdSFndoWAr9h3xtCW3Tiy9QIqTzFy1t9owQn-q6FHBRiz3nuBsfprVGxp5zqpWRFnrL3Ui06PZX3ONHZu-v4LH9f_LnakWGhauIWCAL-JjQl0aG_IEDm2P9wDC-fIVZbZG6AFx2cTLXZ9YHfA04LoD0EvfiSmm2tTPNmu1oSMKoXBob8MEMnITCh4HK2wzRJLnBAW3dNgauHidAOLPT5LWycntjScr55fnSeJTmbd74395KFTwbVr6iEVbXm9mjawvdB7VjTnAxIu4TZJgWakQrqSBUD4ovolbhnNgIMJfz08wAAAAFhVMVfAA")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001837099658"))
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
