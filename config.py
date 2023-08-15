de879c480cc65034be5a54c375e4404203d4e077
fuvkk <93823376+fuvkk@users.noreply.github.com>
2023-08-15 15:00:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     FROM_CHANNEL = os.environ.get("FROM_CHANNEL", "@OkFiles")
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "video")
     OWNER_ID = os.environ.get("OWNER_ID", "2056407064")
-    LIMIT = int(os.environ.get("LIMIT",10000"))
+    LIMIT = int(os.environ.get("LIMIT","10000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
     SESSION = os.environ.get("SESSION", "BQByPmwASpFCKnoXcYJM5MTb8RL8BZUnTvTDvnrOXpegkAZud1M0-2c7P_p1ypncFSDZ2OOuxEbQfYfTfbxJb85SMICbuxKbZ4vD6ZXS1Vr2A6ncED3LsY_sMRKeDmZXC33nVp7CNZMNxMJrrrk7HchMOM5FicxdrIYG_a3vY5oouDvEXj8gNVuQbmIYnbq1Vvn2rFiwKmMhyJVeKPyNeiX6ZnSCefxayecFr051rchgVUHbmG2UzPYk1m3vAOI5eMjz0vXgfQoNvQxc4gPOhtwRfpt-AMcNfysxr_CzYpg-setLROr4RfgqEPDh1SJxMUwBUjz9eOZCrjiOXBtYiBAfUC9zTAAAAAB0nNOMAA")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001837099658"))
